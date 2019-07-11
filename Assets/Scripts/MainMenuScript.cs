d58566894ca1c761872a6a2b320b752962b5349c
Spencer Ng <sng1488@gmail.com>
2019-07-11 15:43:52 -0400

    Add prototype button click script

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.Log("Main menu script working!");
+        Button speechInNoiseButton = gameObject.GetComponent("SpeechInNoiseButton") as Button;
+        speechInNoiseButton.onClick.AddListener(() => SwitchScene(1));
+
+    }
+
+    void SwitchScene(int gameNum)
+    {
+        if (gameNum == 1)
+        {
+            SceneManager.LoadScene("SpeechInNoiseGame", LoadSceneMode.Additive);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Main menu script working!");
+    }
+}
