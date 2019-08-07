a864aa66eec090fbec29249a56c01c0a6109c308
Owais Aftab <oaftab77@gmail.com>
2019-08-07 15:26:55 -0400

    Experiment with recoridng file output

---
+++ Assets/Scripts/AudioTestingScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTestingScript : MonoBehaviour
+{
+    public float[] listenerSamples = new float[128];
+    public float[] sourceSamples = new float [128];
+    public AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+
+        CollectData();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CollectData();
+    }
+
+    public void CollectData()
+    {
+
+        source.GetSpectrumData(sourceSamples, 0, FFTWindow.Blackman);
+        AudioListener.GetSpectrumData(listenerSamples, 0, FFTWindow.Blackman);
+
+        //yield return 3f;
+
+
+    }
+}
