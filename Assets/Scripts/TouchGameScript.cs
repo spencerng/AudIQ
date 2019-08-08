44ad94fc496226a73642681a911c1a692b61deff
Owais Aftab <oaftab77@gmail.com>
2019-08-08 14:05:05 -0400

    Make LocationTracker

--- Assets/Scripts/TouchGameScript.cs
+++ Assets/Scripts/TouchGameScript.cs
@@ -7,11 +7,15 @@ public class TouchGameScript : MonoBehaviour
     private int numTouches;
     private AudioPlayer player;
     private float localizationFactor, pitchFactor;
+    private Transform locationMarkerTransform;
+    private SpriteRenderer locationMarkerSR;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        locationMarkerTransform = GameObject.Find("LocationMarker").GetComponent<Transform>();
+        locationMarkerSR = GameObject.Find("LocationMarker").GetComponent<SpriteRenderer>();
+
         AudioSource audio = GameObject.Find("AudioManager").GetComponent<AudioSource>();
         player = new AudioPlayer(audio);
         player.Play();
@@ -29,48 +33,50 @@ public class TouchGameScript : MonoBehaviour
             Touch latestTouch = Input.GetTouch(Input.touchCount - 1);
             if (latestTouch.phase == TouchPhase.Moved)
             {
+                locationMarkerSR.sprite = Resources.Load("chicky", typeof(Sprite)) as Sprite;
+                locationMarkerTransform.position = latestTouch.position;
+
+
                 //Question: Should people be able to change both pitch and localization at once, or one at a time? I did one at a time
 
-                if (Mathf.Abs(latestTouch.deltaPosition.x) > Mathf.Abs(latestTouch.deltaPosition.y))
-                {
 
-                    //If we want to slowly change through multiple touches:
-                    localizationFactor = localizationFactor + latestTouch.deltaPosition.x / Screen.width * 90; //Changes location like a slider
-                    if (localizationFactor > 90)
-                        localizationFactor = 90;
-                    else if (localizationFactor < -90)
-                        localizationFactor = -90;
+                //If we want to slowly change through multiple touches:
+                localizationFactor = localizationFactor + latestTouch.deltaPosition.x / Screen.width * 90; //Changes location like a slider
+				if (localizationFactor > 90)
+					localizationFactor = 90;
+				else if (localizationFactor < -90)
+					localizationFactor = -90;
 
-                    //Question: Do we care about the angle, or is a binary 90/-90 for the angle OK?
-                    /*
-                    float localizationFactor = latestTouch.deltaPosition.x;
-                    if (localizationFactor > 0)
-                        localizationFactor = 90;
-                    else if (localizationFactor == 0)
-                        localizationFactor = 0;
-                    else if (localizationFactor < 0)
-                        localizationFactor = -90;
-                    */
+				//Question: Do we care about the angle, or is a binary 90/-90 for the angle OK?
+				/*
+				float localizationFactor = latestTouch.deltaPosition.x;
+				if (localizationFactor > 0)
+					localizationFactor = 90;
+				else if (localizationFactor == 0)
+					localizationFactor = 0;
+				else if (localizationFactor < 0)
+					localizationFactor = -90;
+				*/
 
-                    if (Mathf.Abs(localizationFactor) > 5)
-                        player.SetOffsetAngle(-localizationFactor);
+				if (Mathf.Abs(localizationFactor) > 5)
+					player.SetOffsetAngle(-localizationFactor);
 
 
-                }
-                else
-                {
-                    //Question: How much should moving the finger across the screen alter the touch (ie, how big is the pitch range we're trying to test?)
+				
+				//Question: How much should moving the finger across the screen alter the touch (ie, how big is the pitch range we're trying to test?)
 
-                    pitchFactor = pitchFactor + latestTouch.deltaPosition.y / Screen.height / 1.4f; 
-                    //1.1 is for attenuation; will probably need algorithm for proper attenuation
-                    if (pitchFactor < 0) //song starts playing backwards
-                        pitchFactor = 0.05f;
+				pitchFactor = pitchFactor + latestTouch.deltaPosition.y / Screen.height / 1.4f; 
+				//1.1 is for attenuation; will probably need algorithm for proper attenuation
+				if (pitchFactor < 0.5f) //song starts playing backwards
+					pitchFactor = 0.5f;
+                if (pitchFactor > 2f)
+                    pitchFactor = 2f;
 
-                    if (Mathf.Abs(pitchFactor) > 0.05f)
-                        player.SetPitch(pitchFactor);
-                }
+				if (Mathf.Abs(pitchFactor) > 0.05f)
+					player.SetPitch(pitchFactor);
+            
 
-                Debug.Log(pitchFactor);
+                Debug.Log(latestTouch.position);
 
             }
         }
